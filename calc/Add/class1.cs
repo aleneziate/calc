143b29d7c7246f5236f9bac661c30d0c22b5bc4f
alene <alene@ABDALLAH>
2024-12-25 07:55:45 +0300

    Add project files.

---
+++ calc/Add/class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calc.Add
+{
+    public class class1
+    {
+        public double add(double x, double y)
+        {
+            return x + y;
+        }
+        public double add(double a, double b, double c)
+        {
+            return a + b + c;
+
+        }
+        public int add(int num1, int num2 ,int num3)
+        {
+
+            return (num1+ num2 + num3);
+
+        }
+
+        public string salary(double salary, int work_hour)
+        {
+            return $"total salary { salary + (work_hour * 10)}";
+
+
+        }
+
+
+    }
+
+
+}
---
+++ calc/Program.cs
@@ -0,0 +1,52 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using calc.Add;
+
+class1 add = new class1();
+
+Console.WriteLine("function 1");
+Console.WriteLine("eneter 2 number :");
+double x =Convert.ToDouble(Console.ReadLine());
+double y =Convert.ToDouble(Console.ReadLine());
+class sum = new class1();
+Console.WriteLine($"sum: {add.add(x, y)} ");
+
+
+
+Console.WriteLine("-------------------");
+
+Console.WriteLine("function2 ");
+Console.WriteLine( "enter 3 numbers"):;
+double a = Convert.ToDouble(Console.ReadLine());
+double b = Convert.ToDouble(Console.ReadLine());
+double c = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine($"sum: {sum.add(a,b,c)} ");
+
+Console.WriteLine("--------------------");
+
+
+
+
+Console.WriteLine("function3(integers only)");
+Console.WriteLine("enter 3 numbers");
+double num1 = Convert.ToDouble(Console.ReadLine());
+double num2 = Convert.ToDouble(Console.ReadLine());
+double num3 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine($"sum: {add.add(num1,num2,num3)} ");
+
+
+
+
+
+Console.WriteLine("--------------------");
+
+
+
+
+Console.WriteLine("function 4");
+Console.WriteLine("enter your salary");
+double salary = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("enter your working hours");
+int work_hours=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"sum: {add.salary(salary , work_hours)} ");
+
